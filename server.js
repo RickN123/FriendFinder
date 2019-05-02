48697883ab113255abf063a187615bd8785e0ea5
RickN123 <nicolai.richard@gmail.com>
2019-05-02 14:22:01 -0400

    framework

---
+++ server.js
@@ -0,0 +1,27 @@
+
+var express = require("express");
+var path = require("path");
+
+
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+
+});
+
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
