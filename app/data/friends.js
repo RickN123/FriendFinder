2378c6d2650dd823718c79c9498f15c4e1275480
RickN123 <nicolai.richard@gmail.com>
2019-05-02 20:38:33 -0400

    update friend

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var newfriend = [
+
+    {
+        Name: "Johnny Appleseed",
+        Image: "image.img",
+        Q1: "3",
+        Q2: "4",
+        Q3: "3",
+        Q4: "5",
+        Q5: "1",
+        Q6: "2",
+        Q7: "4",
+        Q8: "4",
+        Q9: "3",
+        Q10: "3"
+
+    }
+];
+
+module.exports = newfriend
