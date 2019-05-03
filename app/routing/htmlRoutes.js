045f70ef050bf1760cffabae4a6d3319dfd6f660
RickN123 <nicolai.richard@gmail.com>
2019-05-02 21:05:55 -0400

    update routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,3 +3,17 @@ var path = require("path");
 
 module.exports = function (app) {
 
+    app.get("/tables", function (req, res) {
+
+        res.sendFile(path.join(_dirname, "../public/survey.html"));
+
+    });
+
+    app.get("*", function (req, res) {
+
+        res.sendFile(path.join(_dirname, "../public/home.html"));
+
+    });
+
+
+};
